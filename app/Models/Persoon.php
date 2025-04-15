0f838304bab7d676dc7585b43fc2d13cb6f527e5
NimrodLobozar <nimrod.lobozar@gmail.com>
2025-04-15 09:49:55 +0200

    Add Persoon model: define fillable attributes and establish contact relationship

---
+++ app/Models/Persoon.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class persoon extends Model
+{
+    protected $fillable = [
+        'voornaam',
+        'tussenvoegsel',
+        'achternaam',
+        'is_volwassen',
+        'is_active',
+        'opmerking',
+        'datum_aangemaakt',
+        'datum_gewijzigd',
+    ];
+
+    public function contact()
+    {
+        return $this->hasOne(Contact::class, 'persoon_id', 'id'); // Correct table and column names
+    }
+}
